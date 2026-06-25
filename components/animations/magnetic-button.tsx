e6af0ab558a7dcd2feb38fa1da859cb6a670de08
Shreyansh <kake0shreyansh@gmail.com>
2026-06-25 14:48:10 +0530

    update

--- components/animations/magnetic-button.tsx
+++ components/animations/magnetic-button.tsx
@@ -1,9 +1,9 @@
 "use client"
 
 import React, { useRef, useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, HTMLMotionProps } from 'framer-motion'
 
-interface MagneticButtonProps extends React.HTMLAttributes<HTMLDivElement> {
+interface MagneticButtonProps extends HTMLMotionProps<"div"> {
   children: React.ReactNode
   strength?: number
 }
